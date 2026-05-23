1a5b3b5fdc3ccdd08a50acf451afe5397ef700b1
Khoahocbachkhoa <Khoa.VD235354@sis.hust.edu.vn>
2026-05-23 20:40:49 +0700

    Update schema

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,168 +1,146 @@
 -- todo: Sau này thêm bảo mật
-create table users (
-    id SERIAL PRIMARY KEY,
+
+-- Users
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Thư mục
-create table folders (
+CREATE TABLE folders (
     id BIGSERIAL PRIMARY KEY,
-
-    owner_id INTEGER NOT NULL -- chủ sỡ hữu của thư mục
+    owner_id BIGINT NOT NULL -- người tạo thư mục
         REFERENCES users(id)
         ON DELETE CASCADE,
-
-    parent_id BIGINT -- Thư mục cha
+    parent_id BIGINT         -- thư mục cha
         REFERENCES folders(id)
-        ON DELETE CASCADE, 
+        ON DELETE CASCADE,
 
     name VARCHAR(255) NOT NULL,
-
-    created_by INTEGER
-        REFERENCES users(id),
-
-    updated_by INTEGER
-        REFERENCES users(id),
-
-    deleted_at TIMESTAMP,
-
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-
-    is_root BOOLEAN DEFAULT FALSE
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
 );
+
+CREATE UNIQUE INDEX uq_folder_name -- các thư mục trong cùng 1 dir phải khác tên
+ON folders(owner_id, parent_id, name) 
+WHERE deleted_at IS NULL;
+
+CREATE UNIQUE INDEX uq_root_folder -- một user chỉ được tạo 1 thư mục gốc
+ON folders(owner_id)
+WHERE parent_id IS NULL
+AND deleted_at IS NULL;
 
 -- Tệp tin
 CREATE TABLE files (
     id BIGSERIAL PRIMARY KEY,
 
-    owner_id INTEGER NOT NULL -- chủ sở hữu file
+    owner_id BIGINT NOT NULL
         REFERENCES users(id)
         ON DELETE CASCADE,
 
-    folder_id BIGINT -- Thư mục cha
+    folder_id BIGINT
         REFERENCES folders(id)
         ON DELETE CASCADE,
 
     filename VARCHAR(255) NOT NULL,
 
-    storage_key TEXT NOT NULL, -- định danh file trên storage backend
+    storage_key TEXT NOT NULL UNIQUE,
 
     size BIGINT NOT NULL DEFAULT 0,
 
-    mime_type VARCHAR(255), -- kiểu mime
+    mime_type VARCHAR(255),
 
-    created_by INTEGER
-        REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
+);
 
-    updated_by INTEGER
-        REFERENCES users(id),
+CREATE UNIQUE INDEX uq_file_name
+ON files(owner_id, folder_id, filename) -- Một thư mục chứa các file có tên riêng biệt
+WHERE deleted_at IS NULL;
 
-    deleted_at TIMESTAMP,
+-- Phân quyền
+
+CREATE TABLE folder_permissions (
+    folder_id BIGINT NOT NULL
+        REFERENCES folders(id)
+        ON DELETE CASCADE,
+
+    user_id BIGINT NOT NULL
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    role VARCHAR(20) NOT NULL,
 
     created_at TIMESTAMP DEFAULT NOW(),
 
-    updated_at TIMESTAMP DEFAULT NOW()
+    PRIMARY KEY(folder_id, user_id),
+
+    CONSTRAINT check_folder_role
+    CHECK (role IN ('viewer', 'editor'))
 );
 
--- Phân quyền
-create table permissions (
-    id BIGSERIAL PRIMARY KEY,
-    resource_type VARCHAR(10) NOT NULL, -- file hoặc folder
-    resource_id BIGINT NOT NULL, -- định dạnh file hoặc folder đó
-    target_user_id INTEGER NOT NULL -- chỉ user_id được nhắm tới
+CREATE TABLE file_permissions (
+    file_id BIGINT NOT NULL
+        REFERENCES files(id)
+        ON DELETE CASCADE,
+
+    user_id BIGINT NOT NULL
         REFERENCES users(id)
         ON DELETE CASCADE,
-    role VARCHAR(20) NOT NULL, -- viewer: chỉ được phép list, cd và download file về
-                               -- editor: có thể xóa, sửa, cập nhật file tùy ý
-    created_by INTEGER
-        REFERENCES users(id),
 
-    created_at TIMESTAMP DEFAULT NOW()
+    role VARCHAR(20) NOT NULL,
+
+    created_at TIMESTAMP DEFAULT NOW(),
+
+    PRIMARY KEY(file_id, user_id),
+
+    CONSTRAINT check_file_role
+    CHECK (role IN ('viewer', 'editor'))
 );
 
 -- audit
 CREATE TABLE audit_logs (
     id BIGSERIAL PRIMARY KEY,
 
-    actor_user_id INTEGER
+    actor_user_id BIGINT
         REFERENCES users(id),
 
     action VARCHAR(50) NOT NULL,
-    -- upload
-    -- delete
-    -- rename
-    -- move
-    -- mkdir
 
-    resource_type VARCHAR(10) NOT NULL, -- file hoặc folder
+    resource_type VARCHAR(10) NOT NULL,
     resource_id BIGINT NOT NULL,
 
     metadata JSONB,
 
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT check_resource_type
+    CHECK (resource_type IN ('file', 'folder'))
 );
 
--- Thêm ràng buộc
+-- Chỉ mục
 
--- unique folers name 
-ALTER TABLE folders
-ADD CONSTRAINT unique_folder_name
-UNIQUE(parent_id, owner_id, name);
-
--- unique file name
-ALTER TABLE files
-ADD CONSTRAINT unique_file_name
-UNIQUE(folder_id, owner_id, filename);
-
--- unique permissions
-ALTER TABLE permissions
-ADD CONSTRAINT unique_permission
-UNIQUE (
-    resource_type,
-    resource_id,
-    target_user_id
-);
-
--- Thêm check
-
-ALTER TABLE permissions
-ADD CONSTRAINT check_role
-CHECK (role IN ('viewer', 'editor'));
-
-ALTER TABLE permissions
-ADD CONSTRAINT check_resource_type
-CHECK (resource_type IN ('file', 'folder'));
-
-ALTER TABLE audit_logs
-ADD CONSTRAINT check_audit_resource_type
-CHECK (resource_type IN ('file', 'folder'));
-
--- Thêm chỉ mục
-
-CREATE INDEX idx_folders_parent
+CREATE INDEX idx_folder_parent
 ON folders(parent_id);
 
-CREATE INDEX idx_files_folder
+CREATE INDEX idx_file_folder
 ON files(folder_id);
 
-CREATE INDEX idx_permissions_lookup
-ON permissions(
-    resource_type,
-    resource_id,
-    target_user_id
-);
+CREATE INDEX idx_folder_permission_user
+ON folder_permissions(user_id);
+
+CREATE INDEX idx_file_permission_user
+ON file_permissions(user_id);
 
 CREATE INDEX idx_audit_resource
-ON audit_logs(
-    resource_type,
-    resource_id
-);
+ON audit_logs(resource_type, resource_id);
 
--- Thêm unique
+CREATE INDEX idx_folder_owner_parent
+ON folders(owner_id, parent_id);
 
-CREATE UNIQUE INDEX unique_root_folder
-ON folders(owner_id)
-WHERE parent_id IS NULL;
+CREATE INDEX idx_file_owner_folder
+ON files(owner_id, folder_id);
