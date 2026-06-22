60c023ddb4c56fc7a4ede429eb2b568cfc17b09e
Khoahocbachkhoa <Khoa.VD235354@sis.hust.edu.vn>
2026-06-22 22:38:09 +0700

    Add download engine for client

--- client/src/command/transfer.c
+++ client/src/command/transfer.c
@@ -101,14 +101,200 @@ void cmd_upload(ParsedCommand *cmd) {
 }
 
 void cmd_download(ParsedCommand *cmd) {
-    if (cmd == 1) {
+    if (cmd->argc != 2 && cmd->argc != 4) {
         printf("Usage: get <filename> [--path saved_path]\n");
         return;
     }
 
-    
+    // check argvs
+    const char *filename = cmd->argv[1];
+
+    if (!check_file_name(filename)) {
+        printf("Error: invalid filename\n");
+        return;
+    }
+
+    // thư mục lưu trữ
+    char *tmp = "~/Downloads";
+
+    if (cmd->argc == 4) {
+        if (strcmp(cmd->argv[2], "--path") != 0) {
+            printf("Unknown option: %s\n", cmd->argv[2]);
+            return;
+        }
+        tmp = cmd->argv[3];
+    }
+
+    char fullpath[256];
+    normalize_path(tmp, fullpath, sizeof(fullpath));
+
+    char savepath[512];
+    snprintf(savepath, sizeof(savepath), "%s/%s", fullpath, filename);
+
+    // Gửi request
+    char req[256];
+    snprintf(req, sizeof(req), "DOWNLOAD %s\r\n", filename);
+
+    send_command(state->sockfd, req);
+
+    char res[256];
+    // Đọc dòng phản hồi đầu
+    recv_response_to_buf(state->sockfd, res, sizeof(res));
+
+    int code;
+    char message[32];
+    long filesize;
+
+    // ko co filesize -> co error
+    if (sscanf(res, "%d %s %ld\r\n", &code, message, &filesize) != 3 || code != 150) {
+        printf("%s", res);
+        return;
+    }
+
+    // Mỏ file đích
+    FILE *fp = fopen(savepath, "wb");
+    if (fp == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    // Tải file về máy
+    long received = 0;
+    char buffer[8192];
+
+    while (received < filesize) {
+        size_t need = sizeof(buffer);
+
+        if ((long)need > (filesize - received))
+            need = filesize - received;
+
+        int n = net_recv_all(state->sockfd, buffer, need, 0);
+        if (n < 0) {
+            printf("Download interrupted\nMaybe some error occured or lost connection to server\n");
+            // Xóa file tránh file hỏng
+            remove(savepath);
+            fclose(fp);
+            return;
+        }
+
+        received += n;
+
+        // Ghi ra file
+        fwrite(buffer, 1, n, fp);
+    }
+
+    fclose(fp);
+    printf("Saved to path: %s\n", savepath);
+
+    // Đợi phản hồi success từ server
+    recv_response(state->sockfd);
 }
 
 void cmd_download_shared(ParsedCommand *cmd) {
-    
+    if (cmd->argc != 3 && cmd->argc != 5) {
+        printf("Usage: get-shared <fileid> <savename> [--path <save_dir>]\n");
+        return;
+    }
+
+    int fileid = atoi(cmd->argv[1]);
+
+    if (fileid <= 0) {
+        printf("Invalid file id\n");
+        return;
+    }
+
+    const char *savename = cmd->argv[2];
+    if (!check_file_name(savename)) {
+        printf("Invalid filename\n");
+        return;
+    }
+
+    char *tmp = "~/Downloads";
+
+    if (cmd->argc == 5) {
+        if (strcmp(cmd->argv[3], "--path") != 0) {
+            printf("Unknown option: %s\n", cmd->argv[3]);
+            return;
+        }
+
+        tmp = cmd->argv[4];
+    }
+
+    char fullpath[512];
+
+    normalize_path(tmp, fullpath, sizeof(fullpath));
+
+    char savepath[1024];
+
+    snprintf(savepath, sizeof(savepath), "%s/%s", fullpath, savename);
+
+    // download file
+    char req[256];
+
+    snprintf(req, sizeof(req), "DOWNLOAD_SHARED %d\r\n", fileid);
+
+    send_command(state->sockfd, req);
+
+    char res[256];
+
+    if (recv_response_to_buf(state->sockfd, res, sizeof(res)) < 0) {
+        printf("Connection lost\n");
+        return;
+    }
+
+    int code;
+    char message[64];
+    long filesize;
+
+    // Có lỗi xảy ra khi không có file_size
+    if (sscanf(res, "%d %s %ld\r\n", &code, message, &filesize) != 3 || code != 150) {
+        printf("%s", res);
+        return;
+    }
+
+    FILE *fp = fopen(savepath, "wb");
+
+    if (fp == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    long received = 0;
+    char buffer[8192];
+
+    while (received < filesize) {
+        size_t need = sizeof(buffer);
+
+        if ((long)need > filesize - received) {
+            need = filesize - received;
+        }
+
+        int n = net_recv_all(state->sockfd, buffer, need, 0);
+
+        if (n <= 0) {
+            printf("Download interrupted\n");
+
+            fclose(fp);
+            remove(savepath);
+
+            return;
+        }
+
+        if (fwrite(buffer, 1, n, fp) != (size_t)n) {
+            printf("Write file failed\n");
+
+            fclose(fp);
+            remove(savepath);
+
+            return;
+        }
+
+        received += n;
+    }
+
+    fclose(fp);
+
+    printf("Saved to path: %s\n", savepath);
+    // Đợi phản hồi thành công từ server
+    recv_response(state->sockfd);
 }
